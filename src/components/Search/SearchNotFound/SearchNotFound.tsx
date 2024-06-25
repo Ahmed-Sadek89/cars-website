be2a6a9fb537df99288de65ce1ef0c9ea3ede228
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-06-25 06:03:20 +0200

    make search found and not found pages

--- src/components/Search/SearchNotFound/SearchNotFound.tsx
+++ src/components/Search/SearchNotFound/SearchNotFound.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 import React from 'react'
+import ProductAddForm from './ProductAddForm';
 
 const SearchNotFound = () => {
+
   return (
-    <div className='custom-container2'>
+    <div className='custom-container2 pb-10'>
       <div className='py-10 flex flex-col items-center justify-center text-center relative gap-5'>
         <Image src={'/search/not-found.svg'} alt='not-found' width={300} height={300} className='object-cover' />
         <h2 className='font-bold text-4xl text-custom-black'>This product is not available now</h2>
       </div>
-      <div></div>
+      <ProductAddForm />
     </div>
   )
 }
