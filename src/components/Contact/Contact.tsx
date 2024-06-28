1ec5127424d11a916fe7f07c12816b86baa4e6d4
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-06-28 05:45:04 +0200

    make contact page

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,26 @@
+import Choosing from './Choosing'
+import ContactDetails from './ContactDetails'
+import ContactUserLinks from './ContactUserLinks'
+import MainSection from './MainSection'
+import Touch from './Touch'
+
+const Contact = () => {
+    return (
+        <>
+            <MainSection />
+            <div className="custom-container1 flex items-start gap-10 py-10">
+                <Touch />
+                <div className='flex flex-col gap-5'>
+
+                    <ContactDetails />
+                    <ContactUserLinks />
+                </div>
+            </div>
+            <div className="custom-container1 py-10">
+                <Choosing />
+            </div>
+        </>
+    )
+}
+
+export default Contact
