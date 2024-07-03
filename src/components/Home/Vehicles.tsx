4fe3022a6b1b05c20ac12a3842d149911a958b79
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-07-03 03:42:39 +0200

    make search resopnsive

--- src/components/Home/Vehicles.tsx
+++ src/components/Home/Vehicles.tsx
@@ -8,12 +8,12 @@ const Vehicles = () => {
     const pathname = usePathname()
     return (
         <section className='custom-container2 pt-5 pb-10'>
-            <div className={`grid grid-cols-${vehicles.length} items-center gap-5`}>
+            <div className={`grid grid-cols-${vehicles.length} items-center gap-1 md:gap-5 `}>
                 {
                     vehicles.map((vehicle, index) => (
                         <Link href={vehicle.path} key={index} className='flex flex-col items-center text-center gap-3 cursor-pointer h-[20vh]'>
-                            <div className={`h-[100px] w-[100px] rounded-full flex items-center justify-center  ${pathname === vehicle.path ? "bg-custom-blue" : "bg-[#D9D9D9]"}  `}>
-                                <Image src={vehicle.image} alt={vehicle.name} width={50} height={50} className='object-cover' />
+                            <div className={`h-[50px] w-[50px] md:h-[100px] md:w-[100px] p-1 rounded-full flex items-center justify-center  ${pathname === vehicle.path ? "bg-custom-blue" : "bg-[#D9D9D9]"}  `}>
+                                <Image src={vehicle.image} alt={vehicle.name} width={100} height={100} className='object-contain p-1' />
                             </div>
                             <h5 className='text-custom-black font-normal lg:font-bold text-sm'>{vehicle.name}</h5>
                         </Link>
