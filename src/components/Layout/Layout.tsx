a0dbcf15ce88c333df7308e1de53a987ecd47333
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-07-13 21:09:20 +0200

    adasd2w

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <CarContextProvider>
             <Navbar />
-            <main className='min-h-[calc(100vh-3rem)]'>{children}</main>
+            <main className='min-h-[calc(100vh-3rem)] w-full overflow-hidden'>{children}</main>
             <Footer />
             <CustomBottomNavigation />
         </CarContextProvider>
