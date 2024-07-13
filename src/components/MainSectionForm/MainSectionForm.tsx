36859f047895f1cdb89a98d6e2af864dbb4dbd14
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-07-13 21:37:01 +0200

    adasd2w

--- src/components/MainSectionForm/MainSectionForm.tsx
+++ src/components/MainSectionForm/MainSectionForm.tsx
@@ -31,13 +31,13 @@ const MainSectionForm = ({ setOpen }: {
                 <h6 className='text-white'>FIND PARTS FOR YOUR VEHICLE</h6>
                 <div className='bg-transparent lg:bg-white grid grid-cols-1 lg:grid-cols-4 gap-x-0 lg:gap-x-3 items-center rounded-full p-1 lg:p-4'>
                     <SelectInput data={carModels} value={carModelValue} setValue={setCarModelValue} />
-                    <div className='col-span-2 grid grid-cols-2 items-center gap-x-4 lg:gap-0 my-2 lg:my-0'>
+                    <div className='col-span-2 grid grid-cols-2 items-center gap-x-4 lg:gap-x-0  space-y-1 '>
                         <SelectInput data={carYear} value={carYearValue} setValue={setCarYearValue} />
                         <SelectInput data={carSeries} value={carSeriesValue} setValue={setCarSeriesValue} />
                     </div>
                     <Link
                         href={`/search?car_model=${carModelValue}&car_year=${carYearValue}&car_series=${carSeriesValue}`}
-                        className='flex flex-row items-center justify-center gap-4 bg-[#001783] lg:bg-custom-blue text-white rounded-full w-full h-full py-4 lg:py-0'
+                        className='flex flex-row items-center justify-center gap-4 bg-[#001783] lg:bg-custom-blue text-white rounded-full w-full h-full py-1 sm:py-2 lg:py-0'
                         onClick={handleSearchCar}
                     >
                         <SearchIcon />
