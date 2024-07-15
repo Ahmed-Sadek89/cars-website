4c0b34e3ed474ab87bbfacf6c788692214aaaa58
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-07-15 18:42:08 +0200

    make updates

--- src/components/MainSectionForm/MainSectionForm.tsx
+++ src/components/MainSectionForm/MainSectionForm.tsx
@@ -31,18 +31,16 @@ const MainSectionForm = ({ setOpen }: {
                 <h6 className='text-white'>FIND PARTS FOR YOUR VEHICLE</h6>
                 <div
                     className='bg-transparent lg:bg-white grid grid-cols-1 lg:grid-cols-4  items-center rounded-full p-1 lg:p-4'
-                    style={{ gap: "10px" }}
                 >
                     <SelectInput data={carModels} value={carModelValue} setValue={setCarModelValue} />
-                    <div className='col-span-2 grid grid-cols-2 items-center gap-x-4 lg:gap-x-0  space-y-1 '>
+                    <div className='col-span-2 grid grid-cols-2 items-center'>
                         <SelectInput data={carYear} value={carYearValue} setValue={setCarYearValue} />
                         <SelectInput data={carSeries} value={carSeriesValue} setValue={setCarSeriesValue} />
                     </div>
                     <Link
                         href={`/search?car_model=${carModelValue}&car_year=${carYearValue}&car_series=${carSeriesValue}`}
-                        className='flex flex-row items-center justify-center gap-4 bg-[#001783] lg:bg-custom-blue text-white rounded-full w-full h-full '
+                        className='flex flex-row items-center justify-center bg-[#001783] lg:bg-custom-blue text-white rounded-full w-full h-full '
                         onClick={handleSearchCar}
-                        style={{padding: "20px 0px"}}
                     >
                         <SearchIcon />
                         <span className='capitalize'>search cars</span>
