7e222ba8836b7a62d9b2b7dd47d4ca01f6d0239d
abdomo77 <abdomo772001@gmail.com>
2024-07-16 22:30:12 +0300

    update MainSectionForm

--- src/components/MainSectionForm/MainSectionForm.tsx
+++ src/components/MainSectionForm/MainSectionForm.tsx
@@ -7,7 +7,7 @@ import { carModels, carSeries, carYear } from '@/mocks/searchInputs';
 import { CarContext } from '@/app/context/CarContext';
 import './MainSectionForm.css';
 
-const MainSectionForm = ({ setOpen }: { setOpen: React.Dispatch<React.SetStateAction<boolean>> | null }) => {
+const MainSectionForm: React.FC<MainSectionFormProps> = ({ setOpen }) => {
     const [carModelValue, setCarModelValue] = useState(carModels[0]);
     const [carYearValue, setCarYearValue] = useState(carYear[0]);
     const [carSeriesValue, setCarSeriesValue] = useState(carSeries[0]);
@@ -15,7 +15,7 @@ const MainSectionForm = ({ setOpen }: { setOpen: React.Dispatch<React.SetStateAc
     const carContext = useContext(CarContext);
 
     const handleSearchCar = () => {
-        setOpen ? setOpen(false) : null;
+        if (setOpen) setOpen(false);
         const selectedCar = {
             id: Math.random(),
             name: carModelValue,
@@ -26,22 +26,24 @@ const MainSectionForm = ({ setOpen }: { setOpen: React.Dispatch<React.SetStateAc
     };
 
     return (
-        <section className="main-section-form">
-            <div className="form-container">
-                <h6>FIND PARTS FOR YOUR VEHICLE</h6>
-                <div className="input-container input-container-lg">
+        <section className={styles['main-section-form']}>
+            <div className={styles['main-section-form-container']}>
+                <h6 className={styles['form-title']}>FIND PARTS FOR YOUR VEHICLE</h6>
+                <div className={styles['input-container']}>
                     <SelectInput data={carModels} value={carModelValue} setValue={setCarModelValue} />
-                    <div className="select-input-container">
-                        <SelectInput data={carYear} value={carYearValue} setValue={setCarYearValue} />
-                        <SelectInput data={carSeries} value={carSeriesValue} setValue={setCarSeriesValue} />
-                    </div>
+                </div>
+                <div className={styles['input-container-large']}>
+                    <SelectInput data={carYear} value={carYearValue} setValue={setCarYearValue} />
+                    <SelectInput data={carSeries} value={carSeriesValue} setValue={setCarSeriesValue} />
+                </div>
+                <div className={styles['search-container']}>
                     <Link
                         href={`/search?car_model=${carModelValue}&car_year=${carYearValue}&car_series=${carSeriesValue}`}
-                        className="search-link"
+                        className={styles['search-button']}
                         onClick={handleSearchCar}
                     >
                         <SearchIcon />
-                        <span>search cars</span>
+                        <span className='capitalize'>search cars</span>
                     </Link>
                 </div>
             </div>
@@ -50,3 +52,4 @@ const MainSectionForm = ({ setOpen }: { setOpen: React.Dispatch<React.SetStateAc
 };
 
 export default MainSectionForm;
+
