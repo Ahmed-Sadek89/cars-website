fc3984ce042454fc2ea2e6b7da48125e1a055ee7
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-07-16 07:23:21 +0200

    make css file for the search form

--- src/components/Home/MainSection/MainSection.tsx
+++ src/components/Home/MainSection/MainSection.tsx
@@ -6,6 +6,7 @@ import SelectInput from '@/components/MainSectionForm/SelectInput'
 import Link from 'next/link'
 import { carModels, carSeries, carYear } from '@/mocks/searchInputs'
 import { CarContext } from '@/app/context/CarContext'
+import MainSectionForm from '@/components/MainSectionForm/MainSectionForm';
 
 const MainSection = () => {
     const [carModelValue, setCarModelValue] = useState(carModels[0]);
@@ -27,26 +28,7 @@ const MainSection = () => {
         <main className='relative w-full mb-10 lg:mb-52 flex flex-col gap-5'>
             <MainSectionSlider />
             <section className='relative lg:absolute w-full h-full lg:h-auto bottom-0 lg:bottom-[-160px] flex items-center justify-center'>
-                <div className=' bg-custom-blue rounded-md shadow-xl pb-6 pt-9 px-8 flex flex-col overflow-hidden h-full gap-[10px]'>
-                    <h6 className='text-white'>FIND PARTS FOR YOUR VEHICLE</h6>
-                    <div
-                        className='bg-transparent lg:bg-white grid grid-cols-1 lg:grid-cols-4  items-center rounded-full p-1 lg:p-4 gap-1'
-                    >
-                        <SelectInput data={carModels} value={carModelValue} setValue={setCarModelValue} />
-                        <div className='col-span-2 grid grid-cols-2 items-center'>
-                            <SelectInput data={carYear} value={carYearValue} setValue={setCarYearValue} />
-                            <SelectInput data={carSeries} value={carSeriesValue} setValue={setCarSeriesValue} />
-                        </div>
-                        <Link
-                            href={`/search?car_model=${carModelValue}&car_year=${carYearValue}&car_series=${carSeriesValue}`}
-                            className='flex flex-row items-center justify-center bg-[#001783] lg:bg-custom-blue text-white rounded-full w-full h-full '
-                            onClick={handleSearchCar}
-                        >
-                            <SearchIcon />
-                            <span className='capitalize'>search cars</span>
-                        </Link>
-                    </div>
-                </div>
+                <MainSectionForm setOpen={null} />
             </section>
         </main>
     )
--- src/components/MainSectionForm/MainSectionForm.tsx
+++ src/components/MainSectionForm/MainSectionForm.tsx
@@ -1,14 +1,13 @@
-"use client"
-import React, { useContext, useState } from 'react'
-import SelectInput from './SelectInput'
+"use client";
+import React, { useContext, useState } from 'react';
+import SelectInput from './SelectInput';
 import SearchIcon from '@mui/icons-material/Search';
 import Link from 'next/link';
 import { carModels, carSeries, carYear } from '@/mocks/searchInputs';
 import { CarContext } from '@/app/context/CarContext';
+import './MainSectionForm.css';
 
-const MainSectionForm = ({ setOpen }: {
-    setOpen: React.Dispatch<React.SetStateAction<boolean>> | null
-}) => {
+const MainSectionForm = ({ setOpen }: { setOpen: React.Dispatch<React.SetStateAction<boolean>> | null }) => {
     const [carModelValue, setCarModelValue] = useState(carModels[0]);
     const [carYearValue, setCarYearValue] = useState(carYear[0]);
     const [carSeriesValue, setCarSeriesValue] = useState(carSeries[0]);
@@ -16,39 +15,38 @@ const MainSectionForm = ({ setOpen }: {
     const carContext = useContext(CarContext);
 
     const handleSearchCar = () => {
-        setOpen ? setOpen(false) : null
+        setOpen ? setOpen(false) : null;
         const selectedCar = {
             id: Math.random(),
             name: carModelValue,
             year: carYearValue,
             series: carSeriesValue
-        }
-        carContext?.addCar(selectedCar)
-    }
+        };
+        carContext?.addCar(selectedCar);
+    };
+
     return (
-        <section className='px-[15px] w-full mx-auto h-full'>
-            <div className=' bg-custom-blue rounded-md shadow-xl pb-6 pt-9 px-8 flex flex-col overflow-hidden h-full gap-[10px]'>
-                <h6 className='text-white'>FIND PARTS FOR YOUR VEHICLE</h6>
-                <div
-                    className='bg-transparent lg:bg-white grid grid-cols-1 lg:grid-cols-4  items-center rounded-full p-1 lg:p-4 gap-1'
-                >
+        <section className="main-section-form">
+            <div className="form-container">
+                <h6>FIND PARTS FOR YOUR VEHICLE</h6>
+                <div className="input-container input-container-lg">
                     <SelectInput data={carModels} value={carModelValue} setValue={setCarModelValue} />
-                    <div className='col-span-2 grid grid-cols-2 items-center'>
+                    <div className="select-input-container">
                         <SelectInput data={carYear} value={carYearValue} setValue={setCarYearValue} />
                         <SelectInput data={carSeries} value={carSeriesValue} setValue={setCarSeriesValue} />
                     </div>
                     <Link
                         href={`/search?car_model=${carModelValue}&car_year=${carYearValue}&car_series=${carSeriesValue}`}
-                        className='flex flex-row items-center justify-center bg-[#001783] lg:bg-custom-blue text-white rounded-full w-full h-full '
+                        className="search-link"
                         onClick={handleSearchCar}
                     >
                         <SearchIcon />
-                        <span className='capitalize'>search cars</span>
+                        <span>search cars</span>
                     </Link>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default MainSectionForm
+export default MainSectionForm;
