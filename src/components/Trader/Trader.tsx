688d56d4c2fdea67a081959d9b6001d13f8feb49
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-07-18 21:29:57 +0200

    set trader page

--- src/components/Trader/Trader.tsx
+++ src/components/Trader/Trader.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { TextField, Button, Box, Typography } from '@mui/material';
+import { TextField, Button, Box, Typography, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
 import { styled } from '@mui/system';
 
 const Input = styled('input')({
@@ -7,14 +7,14 @@ const Input = styled('input')({
 });
 
 const Trader = () => {
-
+    const cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
     return (
         <aside className='py-10 flex items-center justify-center'>
 
             <div className='w-5/6 lg:w-2/3  gap-8 flex flex-col items-center justify-center'>
-                <Box className="p-6 bg-white shadow-md rounded-lg ">
+                <Box className="p-6 bg-white shadow-md rounded-lg">
                     <Typography variant="h6" className="mb-4">Upload Required Images and Enter Personal Information</Typography>
-                    <form onSubmit={(e) => e.preventDefault()} className="space-y-4">
+                    <form onSubmit={e => e.preventDefault()} className="space-y-4">
                         <TextField
                             label="Full Name"
                             variant="outlined"
@@ -33,37 +33,69 @@ const Trader = () => {
                             fullWidth
                             required
                         />
-                        <TextField
-                            label="Address"
-                            variant="outlined"
-                            fullWidth
-                            required
-                        />
-                        <div>
-                            <Input
-                                id="id-card-photo-upload"
-                                type="file"
-                                accept="image/*"
-                            />
-                            <label htmlFor="id-card-photo-upload">
-                                <Button variant="contained" component="span">
-                                    Upload ID Card Photo
-                                </Button>
-                            </label>
+
+                        <div className="my-10 flex items-start flex-col lg:flex-row w-full justify-between gap-0 lg:gap-10">
+                            <Typography>Address Details:</Typography>
+                            <div className='w-full flex-1 flex flex-col gap-5'>
+                                <FormControl fullWidth required>
+                                    <InputLabel id="city-select-label">City</InputLabel>
+                                    <Select
+                                        labelId="city-select-label"
+                                    >
+                                        {cities.map((city) => (
+                                            <MenuItem key={city} value={city}>
+                                                {city}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                                <TextField
+                                    label="Street Name"
+                                    variant="outlined"
+                                    fullWidth
+                                    required
+                                />
+                                <TextField
+                                    label="Building Number"
+                                    variant="outlined"
+                                    fullWidth
+                                    required
+                                />
+                                <TextField
+                                    label="District"
+                                    variant="outlined"
+                                    fullWidth
+                                    required
+                                />
+                            </div>
                         </div>
-                        <div>
-                            <Input
-                                id="store-photo-upload"
-                                type="file"
-                                accept="image/*"
-                            />
-                            <label htmlFor="store-photo-upload">
-                                <Button variant="contained" component="span">
-                                    Upload Store Photo
-                                </Button>
-                            </label>
+                        <div className='w-2/3 lg:2-1/3 pt-10 flex flex-col gap-2'>
+                            <div className='w-full'>
+                                <Input
+                                    id="id-card-photo-upload"
+                                    type="file"
+                                    accept="image/*"
+                                />
+                                <label htmlFor="id-card-photo-upload">
+                                    <Button variant="contained" component="span" className='w-full bg-custom-blue'>
+                                        Upload ID Card Photo
+                                    </Button>
+                                </label>
+                            </div>
+                            <div className='w-full'>
+                                <Input
+                                    id="store-photo-upload"
+                                    type="file"
+                                    accept="image/*"
+                                />
+                                <label htmlFor="store-photo-upload">
+                                    <Button variant="contained" component="span" className='w-full bg-custom-blue'>
+                                        Upload Store Photo
+                                    </Button>
+                                </label>
+                            </div>
                         </div>
-                        <Button type="submit" variant="contained" color="primary" fullWidth>
+                        <Button type="submit" variant="contained" color="primary" fullWidth  className='w-full bg-custom-green hover:bg-custom-green'>
                             Submit
                         </Button>
                     </form>
